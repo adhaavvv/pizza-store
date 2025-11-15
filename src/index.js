959e24b235c2b55d9f1d6977bdfb7320cc03bb37
adhav <adhavanbalagan@gmail.com>
2025-11-15 23:20:18 +0800

    L7 First Edit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
+// index.js
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"
+import { pizza } from "./data.js";
 
 function App() {
   return (
     <div className="container">
     <div style={{ textAlign: "center"}}>
       <Header />
-      <div style={{ textAlign: "center"}}>
-        <p style={{ fontFamily: "Courier New", fontSize: "15px"}}>Authentic Italian cuisune, all from our stone oven.</p>
-    <div style={{marginTop: "10px"}}>
-        <Menu />
+      <Tagline />
+      <div style={{marginTop: "10px"}}>
+        <Menu pizzas={pizza}/>
       </div>
-        <div style={{ marginTop: "30px"}}>
+        <div style={{ marginTop: "30px"}}>  
       <Footer /> 
+      <OrderButton />
       </div> 
-      </div>
     </div>
+    </div>
+  );
+}
+
+function OrderButton() {
+  const currentHour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 25;
+  const isOpen = currentHour >= openHour && currentHour < closeHour;
+  return (
+    <div style={{marginTop: "10px"}}>
+    {isOpen ? (<button className='btn' onClick={() => alert("Your order has been placed!")}> Order </button>) : " "}
+    </div>
+  );
+}
+
+function ShopOpenHours() {
+  const currentHour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 25;
+  return currentHour >= openHour && currentHour < closeHour;
+}
+
+function Tagline() {
+  const isOpen = ShopOpenHours();
+
+  return (
+    <div style={{ textAlign: "center"}}>
+      <p style={{ fontFamily: "Courier New", fontSize: "15px"}}>{isOpen ? "Authentic Italian Cuisine" : " "}</p>
     </div>
   );
 }
@@ -24,100 +55,102 @@ function App() {
 function Header() {
   return (
     <div>
-        {/* Activity 5 */}
       <h1 style={{ fontFamily: "Arial Black" , color: "orange", textAlign: "Center", fontSize: "48px", textTransform: "uppercase" }}>Adhav's Pizza Co.</h1>
     </div>
   );
 }
+
+// function Menu({ pizzas }) {
+//   return(
+//     <div>
+//       {pizzas.map((pizza, index) => (
+//         <Pizza
+//             key={index}
+//             name={pizza.name}
+//             ingredients={pizza.ingredients}
+//             price={pizza.price}
+//             photoName={pizza.photoName}
+//             soldOut={pizza.soldOut}
+//         />
+//       ))}
+//     </div>
+//   )
+// }
 
 function Menu() {
     return (
         <div className="menu" style={{ fontFamily:"Courier New" }}>
             <h2>Our Menu</h2>
             <div className="pizzas">
-              <Product />
-              <ProductTwo />
-              <ProductThree />
-              <ProductFour />
-              <ProductFive />
-              <ProductSix />
+              <Pizza 
+                image_source="pizzas/margherita.jpg"
+                image_alt="Margherita Pizza"
+                type="Margherita Pizza"
+                desc="Tomato, Mozzarella Cheese"
+                price="10"> </Pizza>
+                <Pizza 
+                image_source="pizzas/spinaci.jpg"
+                image_alt="Spinaci Pizza"
+                type="Spinaci Pizza"
+                desc="Tomato, Mozzarella Cheese, Spinach"
+                price="12"> </Pizza>
+                <Pizza
+                  image_source="pizzas/funghi.jpg"
+                  image_alt="Funghi Pizza"
+                  type="Funghi Pizza"
+                  desc="Tomato, Mozzarella Cheese, Mushrooms, Onion"
+                  price="12"></Pizza>
+                <Pizza
+                  image_source="pizzas/salamino.jpg"
+                  image_alt="Salamino Pizza"
+                  type="Salamino Pizza"
+                  desc="Tomato, Mozzarella Cheese, Pepperoni"
+                  price="15"></Pizza>
+                <Pizza
+                  image_source="pizzas/prosciutto.jpg"
+                  image_alt="Prosciutto Pizza"
+                  type="Prosciutto Pizza"
+                  desc="Tomato, Mozzarella Cheese, Ham, Aragula, Burrete Cheese"
+                  price="18"></Pizza>
+                <Pizza
+                  image_source="pizzas/focaccia.jpg"
+                  image_alt="Focaccia"
+                  type="Focaccia"
+                  desc="Bread with Italian Olive Oil and Rosemary"
+                  price="6"></Pizza>
+
         </div>
         </div>
 
     )
 }
 
-function Product() {
+function Pizza({ image_source, image_alt, type, desc, price }) {
     return (
-      <div className = "pizza" >
-          <img src="margherita.jpg" alt="Margherita Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-          <h3>Margherita Pizza</h3> 
-          <p>Tomato, Mozzarella Cheese</p>
-          <p>$10</p>
+      <div className="pizza">
+        <img src={image_source} alt={image_alt} width="150" style={{ borderRadius:"10px"}}></img>
+        <h3>{type}</h3>
+        <p>{desc}</p>
+        <p>${price}</p>
       </div>
     );
   }
 
-function ProductTwo() {
-  return (
-    <div className = "pizza">
-        <img src="spinaci.jpg" alt="Spinaci Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-        <h3>Spinach Pizza</h3> 
-        <p>Tomato, Mozzarella Cheese, Spinach</p>
-        <p>$12</p>
-    </div>
-  );
-}
+// function Pizza({ name, ingredients, price, photoName, soldOut }) {
+//   return (
+//     <div className="pizza">
+//       <img src={photoName} alt={name} width="150" style={{ borderRadius:"10px"}} />
+//       <h3>{name}</h3>
+//       <p>{ingredients}</p>
+//       <p>${price}</p>
+//       {soldOut && <p style={{color:"red"}}>Sold Out</p>}
+//     </div>
+//   );
+// }
 
-function ProductThree() {
-  return (
-    <div className = "pizza">
-        <img src="funghi.jpg" alt="Funghi Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-        <h3>Funghi Pizza</h3> 
-        <p>Tomato, Mozzarella Cheese, Mushrooms, Onion</p>
-        <p>$12</p>
-    </div>
-  );
-}
-
-function ProductFour() {
-  return (
-    <div className = "pizza">
-        <img src="salamino.jpg" alt="Salamino Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-        <h3>Salamino Pizza</h3> 
-        <p>Tomato, Mozzarella Cheese, Pepperoni</p>
-        <p>$15</p>
-    </div>
-  );
-}
-
-function ProductFive() {
-  return (
-    <div className = "pizza">
-        <img src="prosciutto.jpg" alt="Prosciutto Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-        <h3>Prosciutto Pizza</h3> 
-        <p>Tomato, Mozzarella Cheese, Ham, Aragula, Burrete Cheese</p>
-        <p>$18</p>
-    </div>
-  );
-}
-
-function ProductSix() {
-  return (
-    <div className = "pizza">
-        <img src="focaccia.jpg" alt="Focaccia" width="150" style={{ borderRadius:"10px"}}></img>
-        <h3>Focaccia</h3> 
-        <p>Bread with Italian Olive Oil and Rosemary</p>
-        <p>$6</p>
-    </div>
-  );
-}
 
 function Footer() {
-    const currentHour = new Date().getHours();
-    const openHour = 10;
-    const closeHour = 22;
-    const isOpen = currentHour >= openHour && currentHour < closeHour;
+    const isOpen = ShopOpenHours();
   
     return (
       <footer className="footer">
@@ -128,105 +161,3 @@ function Footer() {
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-
-// function App() {
-//   return (
-//     <div style={{ textAlign: "center"}}>
-//       <Header />
-//       <div style={{ textAlign: "center"}}>
-//         <p style={{ fontFamily: "Courier New"}}>Authentic Italian cuisune, all from our stone oven.</p>
-//       <div style={{display: "flex", justifyContent: "Center", flexWrap:"wrap", gap: "60px", "marginTop": "30px", maxWidth:"800px", marginLeft:"auto", marginRight:"auto"}}>
-//           <Product />
-//           <ProductTwo />
-//           <ProductThree />
-//           <ProductFour />
-//           <ProductFive />
-//           <ProductSix />
-//           </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// function Header() {
-//   return (
-//     <div>
-//       <h1 style={{ fontFamily: "Arial Black" , color: "#FFBF00", textAlign: "Center", style: "font-size:30px"}}>ADHAV'S PIZZA CO.</h1>
-//     </div>
-//   );
-// }
-
-// function Product() {
-//     return (
-//       <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//           <img src="margherita.jpg" alt="Margherita Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-//           <h3>Margherita Pizza</h3> 
-//           <p>Tomato, Mozzarella Cheese</p>
-//           <p>$10</p>
-//       </div>
-//     );
-//   }
-
-// function ProductTwo() {
-//   return (
-//     <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//         <img src="spinaci.jpg" alt="Spinaci Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-//         <h3>Spinach Pizza</h3> 
-//         <p>Tomato, Mozzarella Cheese, Spinach</p>
-//         <p>$12</p>
-//     </div>
-//   );
-// }
-
-// function ProductThree() {
-//   return (
-//     <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//         <img src="funghi.jpg" alt="Funghi Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-//         <h3>Funghi Pizza</h3> 
-//         <p>Tomato, Mozzarella Cheese, Mushrooms, Onion</p>
-//         <p>$12</p>
-//     </div>
-//   );
-// }
-
-// function ProductFour() {
-//   return (
-//     <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//         <img src="salamino.jpg" alt="Salamino Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-//         <h3>Salamino Pizza</h3> 
-//         <p>Tomato, Mozzarella Cheese, Pepperoni</p>
-//         <p>$15</p>
-//     </div>
-//   );
-// }
-
-// function ProductFive() {
-//   return (
-//     <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//         <img src="prosciutto.jpg" alt="Prosciutto Pizza" width="150" style={{ borderRadius:"10px"}}></img>
-//         <h3>Prosciutto Pizza</h3> 
-//         <p>Tomato, Mozzarella Cheese, Ham, Aragula, Burrete Cheese</p>
-//         <p>$18</p>
-//     </div>
-//   );
-// }
-
-// function ProductSix() {
-//   return (
-//     <div style={{ fontFamily:"Courier New", textAlign:"Center", width:"250px"}}>
-//         <img src="focaccia.jpg" alt="Focaccia" width="150" style={{ borderRadius:"10px"}}></img>
-//         <h3>Focaccia</h3> 
-//         <p>Bread with Italian Olive Oil and Rosemary</p>
-//         <p>$6</p>
-//     </div>
-//   );
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
